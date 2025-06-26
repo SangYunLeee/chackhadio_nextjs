47343c8d4e414dc6d462a031a43733edb9d028b9
2sangyun <sororiri@gmail.com>
2025-06-26 22:00:55 +0900

    fix: about page 를 video 로 보이도록 수정

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko" className="h-full overflow-hidden">
       <body className="bg-white text-gray-900 flex flex-col h-full overflow-hidden">
         <NavBar />
-        <div className="flex-grow flex flex-col items-center mt-10 sm:mt-0">
+        <div className="flex-grow flex flex-col items-center sm:mt-10 sm:mt-0">
           {children}
         </div>
       </body>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,41 +17,25 @@ export default function AboutPage() {
       </div> */}
         <div className="text-lg text-gray-700 leading-relaxed grid grid-cols-1 gap-x-24 lg:grid-cols-[auto_1fr]">
           <div className="mt-0 sm:mt-20">
-            {/* Main Title */}
-            <div className="text-2xl font-bold mb-10 pb-1 border-b-3 border-gray-200 inline-block">
-              <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl whitespace-nowrap">당신의 이야기를 실현 시켜줄</div>
-              <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl whitespace-nowrap">탁월한 영상 파트너가 필요하다면,</div>
-            </div>
-            {/* Contact Info */}
-            <div className="text-[1rem] sm:text-[1.2rem] md:text-[1.2rem] lg:text-[1.3rem]">
-              <div>
-                {content.about.address}
-              </div>
-              <div className="grid grid-cols-1 sm:grid-cols-1 gap-0">
-                <div className="">
-                  <span>TEL</span>
-                  <span className="mx-2 text-gray-300">❘</span>
-                  <span>{content.about.contact.phone[0].phone}</span>
-                  <span className="mx-2 text-gray-600">·</span>
-                  <span>{content.about.contact.phone[1].phone}</span>
-                </div>
-                <div className="flex items-center">
-                  <span>E-mail</span>
-                  <span className="mx-2 text-gray-300">❘</span>
-                  <span>{content.about.contact.email}</span>
-                </div>
-                <div className="flex items-center">
-                  <span>Business license</span>
-                  <span className="mx-2 text-gray-300">❘</span>
-                  <span>{content.about.contact.license}</span>
-                </div>
-              </div>
-            </div>
-            
+            <video
+              autoPlay
+              muted
+              playsInline
+              className="w-full md:max-w-[480px] lg:max-w-[640px] xl:max-w-[920px] flex items-center justify-center my-5 mx-auto"
+            >
+              <source src="/videos/text_1.webm" type="video/webm" />
+            </video>
           </div>
           {/* Image */}
-          <div className="flex items-center justify-center my-5">
-            <Image src="/images/body_logo.png" alt="logo" width={300} height={300} className="my-auto opacity-30 w-[150px] sm:w-[300px]"/>          
+          <div className="flex items-center justify-center my-5 mt-20">
+            <video
+              autoPlay
+              muted
+              playsInline
+              className="max-h-[10rem] md:max-h-[20rem] flex items-center justify-center my-5"
+            >
+              <source src="/videos/logo_1.webm" type="video/webm" />
+            </video>
           </div>
         </div>
 
